257882270c9102f59c780208029048de538bbdc3
OliverV4 <127059434+19102jhc@users.noreply.github.com>
2023-03-17 00:05:48 +0000

    Did up the loop to redo the quiz at the end

--- main.py
+++ main.py
@@ -29,13 +29,12 @@ Enter_to_play = input("Please type 'ENTER' to start the quiz>>")
   
 if Enter_to_play == 'ENTER':
   print("All right!")
-  
   print (Music_list_dict1)
   answer = input(">>")
   if answer == (Music_list_dict2):
     print ("Well done!")
     score = +5
-    print("You now have 5 points!")
+    print("You now have 5 more points!")
     
 else:
   print("Invalid input, please try again.")
@@ -56,8 +55,11 @@ while True:
   again = input("Do you want to repeat the quiz?")
   if again == 'yes':
     print("Welcome back!")
+    if again =='no':
+      print("Goodbye!")
   else:
-    break
+      print("I don't understand!")
+      break #exit
     
 
   
