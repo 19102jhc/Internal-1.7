ca1155468537d9df8b532b893fd0b92995f33e25
OliverV4 <127059434+19102jhc@users.noreply.github.com>
2023-03-21 02:03:36 +0000

    Fixing the random questions more

--- main.py
+++ main.py
@@ -33,23 +33,24 @@ Music_list_dict1 = {
 'Question 3 ' : {'Question' : 'What band has been going for the longest time live?', 'Answer': 'The Rolling Stones'},
 
 'Question 4 ' : {'Question' :'What is the most hated music genre?', 'Answer': 'Jazz'}
+}
 
 Enter_to_play = input("Please type 'ENTER' to start the quiz>>")
   
 if Enter_to_play == 'ENTER':
   print("All right!")
 #randomly generated questions
-  while score <30:
+ while score < 30:
     random_music_questions = random.choice(list(Music_list_dict1.values()))
-    print (random_music_questions["Question"])
-  answer = input(">>")
-  if answer.lower() == random_music_questions["Answer"].lower():
-    print ("Well done! You got this right!")
-    score = +5
-    print("You now have 5 more points!")
-    print("Your score is now at", score)
-  else:
-    print("Incorrect!")
+    print(random_music_questions["Question"])
+    answer = input(">>")
+    if answer.lower() == random_music_questions["Answer"].lower():
+        print("Well done! You got this right!")
+        score +=5
+        print("You now have 5 more points!")
+        print("Your score is now at", score)
+    else:
+        print("Incorrect!")
     #end of game
 while True:
   if score == 30:
