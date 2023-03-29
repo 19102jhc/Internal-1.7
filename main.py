09fbb01b0e3b6b5f3e9c4eb8fbff6f840ffea577
OliverV4 <127059434+19102jhc@users.noreply.github.com>
2023-03-29 20:28:25 +0000

    Chatgpt helped me use PEP8 to make the code better

--- main.py
+++ main.py
@@ -16,34 +16,33 @@ print("🎵🎶🎹🎸")
 print("You need to get 30 points to pass the quiz! You have 4 chances!")
 time.sleep(1)
 print("Loading...")
-print('Starting in: ')
+print('Starting in:')
+
 # countdown
 for delay in range(5, 0, -1):
-  time.sleep(1)
-  print(delay)
+    time.sleep(1)
+    print(delay)
 
 time.sleep(3)
 print("Score is currently at 0")
 
 score = 0
 chances = 4
+
 # dictionary
-Music_list_dict1 = {
-    'Question 1 ': {'Question': 'Who made the album The Dark Side of The Moon?', 'Answer': 'Pink Floyd'},
-
-    'Question 2 ': {'Question': 'Who is the lead singer of Led Zepplin?', 'Answer': 'Robert Plant'},
-
-    'Question 3 ': {'Question': 'What band has been going for the longest time live?', 'Answer': 'The Rolling Stones'},
-
-    'Question 4 ': {'Question': 'What is the most hated music genre?', 'Answer': 'Jazz'},
-
-    'Question 5 ': {'Question': 'What is the best selling album of all time?', 'Answer': 'Thriller'},
-    'Question 6 ': {'Question': 'What is The Beatles first album?', 'Answer': 'Please Please Me'},
+music_list_dict = {
+    'Question 1': {'Question': 'Who made the album The Dark Side of The Moon?', 'Answer': 'Pink Floyd'},
+    'Question 2': {'Question': 'Who is the lead singer of Led Zepplin?', 'Answer': 'Robert Plant'},
+    'Question 3': {'Question': 'What band has been going for the longest time live?', 'Answer': 'The Rolling Stones'},
+    'Question 4': {'Question': 'What is the most hated music genre?', 'Answer': 'Jazz'},
+    'Question 5': {'Question': 'What is the best selling album of all time?', 'Answer': 'Thriller'},
+    'Question 6': {'Question': 'What is The Beatles first album?', 'Answer': 'Please Please Me'}
 }
+
 # loop to start the quiz so you cannot just randomly put anything
 while True:
-    Enter_to_play = input("Please type 'ENTER' to start the quiz>>")
-    if Enter_to_play == 'ENTER':
+    enter_to_play = input("Please type 'ENTER' to start the quiz>>")
+    if enter_to_play == 'ENTER':
         print("All right!")
         name = input("What is your name?>>   ")
         print("Welcome", name)
@@ -72,7 +71,7 @@ while True:
             print("Goodbye! Thanks for playing!")
             break
     else:
-        random_music_questions = random.choice(list(Music_list_dict1.values()))
+        random_music_questions = random.choice(list(music_list_dict.values()))
         print(random_music_questions["Question"])
         answer = input(">>")
         if answer.lower() == random_music_questions["Answer"].lower():
@@ -85,7 +84,7 @@ while True:
             print("\033[1;31;40m That is incorrect!  \n")
             print("\033[0m.........................................................\n")
             chances -= 1
-        print("You have", chances, "Chances left ! ")
+        print("You have", chances, "Chances left!")
 
 
     
