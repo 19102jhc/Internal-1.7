94f6d8091cbbea6a60f623f059832152bd3eb547
OliverV4 <127059434+19102jhc@users.noreply.github.com>
2023-03-29 20:53:43 +0000

    Changed a few things so I added a code at the bottom in which says how many chances you have left.

--- main.py
+++ main.py
@@ -9,22 +9,31 @@
 #imports
 import time
 import random
-def welcome():
-  print("Welcome to the Music Quiz!")
-print("By Oliver!")
-print("🎵🎶🎹🎸")
-print("You need to get 30 points to pass the quiz! You have 4 chances!")
-time.sleep(1)
-print("Loading...")
-print('Starting in:')
-
-# countdown
-for delay in range(5, 0, -1):
+def welcome_message():
+    print("Welcome to the Music Quiz!")
+    print("By Oliver!")
+    print("🎵🎶🎹🎸")
+    print("You need to get 30 points to pass the quiz! You have 4 chances!")
     time.sleep(1)
-    print(delay)
+    print("Loading...")
+    print('Starting in:')
+    # countdown
+    for delay in range(5, 0, -1):
+        time.sleep(1)
+        print(delay)
+    time.sleep(3)
+    print("Score is currently at 0")
 
-time.sleep(3)
-print("Score is currently at 0")
+def play_again():
+    again = input("Do you want to repeat the quiz?")
+    if again.lower() == 'yes':
+        return True
+    else:
+        print("Goodbye! Thanks for playing!")
+        return False
+
+# tells the welcome function to show the welcome message
+welcome_message()
 
 score = 0
 chances = 4
@@ -36,15 +45,13 @@ music_list_dict = {
     'Question 3': {'Question': 'What band has been going for the longest time live?', 'Answer': 'The Rolling Stones'},
     'Question 4': {'Question': 'What is the most hated music genre?', 'Answer': 'Jazz'},
     'Question 5': {'Question': 'What is the best selling album of all time?', 'Answer': 'Thriller'},
-    'Question 6': {'Question': 'What is The Beatles first album?', 'Answer': 'Please Please Me'}
-  {
-  'Question 7': {'Question': 'What symbol was on Ed Sheerans 2011 album?', 'Answer': '+'},
+    'Question 6': {'Question': 'What is The Beatles first album?', 'Answer': 'Please Please Me'},
+    'Question 7': {'Question': 'What symbol was on Ed Sheerans 2011 album?', 'Answer': '+'}
 }
 
 # loop to start the quiz so you cannot just randomly put anything
 while True:
-
-     enter_to_play = input("Please type 'ENTER' to start the quiz>>")
+    enter_to_play = input("Please type 'ENTER' to start the quiz>>")
     if enter_to_play == 'ENTER':
         print("All right!")
         name = input("What is your name?>>   ")
@@ -57,21 +64,17 @@ while True:
 while True:
     if score == 30:
         print("You have passed the quiz! Well done!")
-        again = input("Do you want to repeat the quiz? (yes or no) ")
-        if again.lower() == 'yes':
+        if play_again():
             score = 0
             chances = 4
         else:
-            print("Goodbye! Thanks for playing!")
             break
     elif chances == 0:
-        print("You have run out of chances! Game over!")
-        again = input("Do you want to repeat the quiz? (yes or no) ")
-        if again.lower() == 'yes':
+        print("You have lost all of your chances!")
+        if play_again():
             score = 0
             chances = 4
         else:
-            print("Goodbye! Thanks for playing!")
             break
     else:
         random_music_questions = random.choice(list(music_list_dict.values()))
@@ -87,8 +90,7 @@ while True:
             print("\033[1;31;40m That is incorrect!  \n")
             print("\033[0m.........................................................\n")
             chances -= 1
-        print("You have", chances, "Chances left!")
-
+        print("You now have", chances, "chances left!")
 
     
 
